c26ee6bee0c0df7aeb4ebbcd89d07b45ae445ca4
Mr.Panda <xivistudios@gmail.com>
2018-05-26 02:46:05 +0800

    panda
    revise request host

--- service/mcuApiService.js
+++ service/mcuApiService.js
@@ -23,6 +23,7 @@ const fs = require("fs")
 const path = require("path")
 const os = require("os")
 const axios = require("axios")
+const uuid = require("uuid/v4")
  
  
  /**
@@ -43,7 +44,7 @@ class mcuApiService {
   afflux ({ cluster, method, data = {}, params = {}, path, port }) {
     return new Promise(async (resolve, reject) => {
       try {
-        let url = cluster.api.protocol + "://127.0.0.1:" + (port || cluster.api.port) + path
+        let url = cluster.api.protocol + "://" + cluster.remoteAddress + ":" + (port || cluster.api.port) + path
         let Authorization = "MAuth realm=http://marte3.dit.upm.es,mauth_signature_method=HMAC_SHA256,mauth_serviceid="
         let cnounce = Math.floor(Math.random() * 99999)
         let timestamp = new Date().getTime()
@@ -65,8 +66,58 @@ class mcuApiService {
    * Token Auth类
    * @private
    */
-  auth () {
-    
+  auth (user, old) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        assert.deepEqual(old !== null && old !== undefined, true, "认证失败")
+        let session = await this.authNext(user, old)
+        user.session = session
+        this.dbService.RedisClient.set("use-" + user.username, JSON.stringify(user))
+        resolve(session)
+      } catch (error) {
+        console.log(error)
+        reject(error)
+      }
+    })
+  }
+  
+  /**
+   * 认证链
+   * @private
+   */
+  authNext (user, old) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        let haxStr = Buffer.from(old, "base64").toString("utf8")
+        let oldSession = await this.include.encrypt(haxStr)
+        let usernameParse = oldSession.match(/^\[.*?\]/g)
+        let username = usernameParse[0].substring(1, usernameParse[0].length - 1)
+        assert.deepEqual(user.key, username, "认证失败")
+        assert.deepEqual(user.session, old, "认证失败")
+        let session = await this.authLoad(user.key)
+        resolve(session)
+      } catch (error) {
+        reject(error)
+      }
+    })
+  }
+  
+  /**
+   * 初始化session
+   * @private
+   */
+  authLoad (key) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        let uid = uuid()
+        let timestamp = new Date().getTime()
+        let session = "[" + key + "][" + uid + "][" + String(timestamp) + "]"
+        let hax = await this.include.decrypt(session)
+        resolve(Buffer.from(hax).toString("base64"))
+      } catch (error) {
+        reject(error)
+      }
+    })
   }
   
 }
